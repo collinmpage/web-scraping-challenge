435d35bf57ceb9aa4ced11d8845d12848a6e25d1
Collin Page <collinmpage@gmail.com>
2021-03-27 19:19:54 -0500

    moving files into the missions to mars folder

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 # Use flask_pymongo to set up mongo connection locally 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "."
 mongo = PyMongo(app)
 
 # Create route that renders index.html template and finds documents from mongo
