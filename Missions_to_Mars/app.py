a3e570f5243b48258d55d7da61b9cc58236cd3cf
Collin Page <collinmpage@gmail.com>
2021-03-31 21:31:26 -0500

    pushing with errors

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection locally 
 app.config["MONGO_URI"] = "."
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Create route that renders index.html template and finds documents from mongo
 @app.route("/")
@@ -30,13 +30,15 @@ def scrape():
 
     # Run scrapped functions
     mars_info = mongo.db.mars_info
-    mars_data = scrape_mars.scrape_mars_news()
-    mars_data = scrape_mars.scrape_mars_image()
-    mars_data = scrape_mars.scrape_mars_facts()
-    mars_data = scrape_mars.scrape_mars_hemispheres()
+    mars_info = scrape_mars.scrape_mars_news()
+    mars_info = scrape_mars.scrape_mars_image()
+    mars_info = scrape_mars.scrape_mars_facts()
+    mars_info = scrape_mars.scrape_mars_hemispheres()
     mars_info.update({}, mars_data, upsert=True)
 
     return redirect("/", code=302)
 
 if __name__ == "__main__": 
     app.run(debug= True)
+
+    #Unclear how to remove the errors from the app.py
